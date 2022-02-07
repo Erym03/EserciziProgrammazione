d875d5bbff0f5107cea52f5137f8ef4fa1a840da
Erym03 <2.carem03@gmail.com>
2022-02-07 15:28:26 +0100

    overloading ++ esercizio

--- Prove di laboratorio 2/Prova2_30-1-22/Origine.cpp
+++ Prove di laboratorio 2/Prova2_30-1-22/Origine.cpp
@@ -15,14 +15,13 @@ public:
 		s = new char[len];
 
 		char vocabolario[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z'};
-		srand(time(0));
 
 		for(int i=0; i<len; i++)
 		{
 			int val = rand() % 26;
 			char carattere = vocabolario[val];
 
-			s[len] = carattere;
+			s[i] = carattere;
 		}
 	}
 
@@ -31,6 +30,18 @@ public:
 	short getLen()
 	{
 		return len;
+	}
+
+	//ostream print A
+	virtual ostream& print(ostream& os)
+	{
+		cout << "S = ";
+		for(int i=0; i<len; i++)
+		{
+			cout << s[i];
+		}
+		cout << " ";
+		return os;
 	}
 
 protected:
@@ -78,6 +89,23 @@ public:
 
 	}
 
+	//ostream print B
+	virtual ostream& print(ostream& os)
+	{
+		A::print(os);
+		cout << "p = " << p;
+		cout << " elab(0) = " << elab(0) << endl;
+
+		return os;
+	}
+
+	B operator++ ()
+	{
+		//Quando incrementiamo con il prefisso ++ un oggetto B aumenterà di 1 la variabile int p
+		p++;
+		return *this; //OK, i tre numeri risulteranno incrementati, rest. una copia
+	}
+
 protected:
 
 	bool test()
@@ -99,6 +127,17 @@ protected:
 	
 };
 
+/*	OVERLOADING ++ E -- dato un oggetto Vettore3D
+
+//1. Operatore unario PREFISSO implementato come MEMBRO
+Vettore3D &Vettore3D::operator ++ ()
+  {
+	x++;
+	y++;
+	z++;
+	return *this; //OK, i tre numeri risulteranno incrementati, rest. una copia
+  }
+*/
 
 class C : public A
 {
@@ -127,9 +166,60 @@ public:
 		ritorno + ritorno + x + c;
 		return ritorno;
 	}
+
+	//ostream print B
+	virtual ostream& print(ostream& os)
+	{
+		A::print(os);
+		cout << "x = " << x;
+		cout << " elab(3) = " << elab(3) << endl;
+
+		return os;
+	}
 };
+
+/*
+
+//2. Operatore unario POSTFISSO implementato come MEMBRO
+Vettore3D Vettore3D::operator ++ (int) // int - parametro ``dummy'' si usa per distinguere le due versioni
+  {
+	Vettore3D aux = *this; //aux e' una copia
+	x++; // incremento
+	y++;
+	z++;
+	return aux; // x,y e z in aux sono invariati
+  }
+
+*/
+
+//Overloading <<
+ostream &operator<<(ostream& os, A& a)
+{
+	return a.print(os);
+}
 
 int main()
 {
+	
+	A *vett[50];
+	srand (111222333);
+	for (int i = 0; i < 50; i++) {
+		short n = 1 + rand() % 10;
+		if(rand() % 2 == 0)
+			vett[i] = new B(n, rand() % 10 + 1);
+		else
+			vett[i] = new C(n, (char)(rand() % ((int)'z' - (int)'a' + 1) + (int)'a'));
+	}	
 
+	
+	//Visualizza tutti i valori
+	for (int i = 0; i < 50; i++) {
+		cout << *vett[i] << endl;
+	}
+
+	//Converte un oggetto di tipo A ad un tipo B per richiamare le funzioni di B
+	static_cast<B*>(vett[0]++);
 }
+
+
+
